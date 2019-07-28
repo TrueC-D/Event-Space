c38fc7d9cb6a33b18561a58adeb3c42b319e8eef
Alethea Cary <aletheacary@gmail.com>
2019-07-28 10:36:11 +0000

    user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
+require './config/environment'
 class ApplicationController <Sinatra::Base
-configure do 
-  enable :sessions
-  set :session_secret, "secret"
+  configure do 
+    enable :sessions
+    set :session_secret, "secret"
+  end
   
   get '/' do 
     erb :index
   end
-  
   
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+    erb :'users/signup'
+    end
+  end
+  
+  post '/signup' do
+    if params[:username] == ''|| params[:password] == ''|| params[:email] == ''
+      redirect '/signup'
+    else User.create(username: params[:username], email: params[:email], password: params[:password])
+      redirect '/tweets'
+    end
+  end
+  
+  get '/login' do
+    erb :'users/login'
+  end
+  post '/login' do 
+    redirect '/account'
+  end
+  
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+end
