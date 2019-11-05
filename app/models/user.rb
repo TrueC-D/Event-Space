5c918f8cda1efe0d9c4474c46d0e85f3c78a6110
Alethea Cary <aletheacary@gmail.com>
2019-11-05 23:43:53 +0000

    slug in usermodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User <ActiveRecord::Base
   has_many :attendee_lists
   has_many :events, through: :attendee_lists
+  
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+  
+  def self.find_by_slug(slug)
+    self.all.find{|user| user.slug == slug}
+  end
+  
 end
   
