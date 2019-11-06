aad200817bc5682d57c0bbd36bcd96d449d0cb13
Alethea Cary <aletheacary@gmail.com>
2019-11-06 02:06:52 +0000

    edit models?

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
-  has_many :attendee_lists
-  has_many :users, through: :attendee_lists
+  has_many :attendee_lists #should be changed to ":attendees"
+  belongs_to :user
+  # has_many :attendee_lists
+  # has_many :users, through: :attendee_lists
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User <ActiveRecord::Base
-  has_many :attendee_lists
-  has_many :events, through: :attendee_lists
+  has_many :events
+  has_many :attendee_lists, through: :events #should be changed to attendees
+  # has_many :attendee_lists
+  # has_many :events, through: :attendee_lists
   
   def slug
     self.username.downcase.gsub(" ", "-")
