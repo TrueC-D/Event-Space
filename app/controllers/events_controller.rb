671a6e08c0c516f8693124b5191741aa48e20e09
Alethea Cary <aletheacary@gmail.com>
2019-11-06 02:20:38 +0000

    event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,19 @@
 class EventsController < ApplicationController
-
+  
   
   get '/events' do 
     erb :'events/event_index'
   end
-end
+  
+  get '/events/:id' do
+    @event = Event.find_by_id(params[:id])
+    if @event 
+      erb :'events/show_event'
+    else
+      redirect 'login'
+    end
+  end
+  
+    
 
 # <a href "/my_events">Home</a> <a href "/events">Events Trending</a> <a href "/login">Login</a> <a href "/logout">Logout</a> 
