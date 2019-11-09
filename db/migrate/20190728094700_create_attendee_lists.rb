dce9ee889b6b52d47733fc537e62f612ffba7c75
Alethea Cary <aletheacary@gmail.com>
2019-11-09 08:30:07 +0000

    events attending - views file

--- db/migrate/20190728094700_create_attendee_lists.rb
+++ db/migrate/20190728094700_create_attendee_lists.rb
@@ -1,6 +1,6 @@
-class CreateAttendeeLists < ActiveRecord::Migration[5.2]
+class CreateEventAttendees < ActiveRecord::Migration[5.2]
   def change
-    create_table :attending do |t|
+    create_table :event_attendees do |t|
       t.integer :user_id
       t.integer :event_id
       t.timestamps
