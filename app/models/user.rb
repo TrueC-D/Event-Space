76677ba2827c6143ce9e6d2e4894903f553d31c7
Alethea Cary <aletheacary@gmail.com>
2019-11-26 20:59:53 +0000

    instructor suggestion - polymorphic associations for events attending

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User <ActiveRecord::Base
   has_secure_password
   has_many :events
   has_many :event_attendees
+  # has_many :events_attending, through: :event_attendees, source: :event
+  # This polymorphic association works better than the events_attending method and works with the views page (but I would have to change other edit and delete methods to not break things)
   
   def events_attending
     EventAttendee.all.select{|item| item.user == self}.collect{|item| item.event}
